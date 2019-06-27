610d528ffd6bb6f421501e37df5c1d33977338c9
Ben Morgan <ben.morgan@warwick.ac.uk>
2019-06-27 16:06:27 +0100

    Create Art/Root binding to SNRawDataProducts
    Add classes/classes_def file to declare trigger_record component
    of SNRawDataProducts to Art/Root, and use art_dictionary to build
    the dictionary library frontend and associated files.
    
    Use include_directories so that art_dictionary can find the
    headers, as target usage properties are not supported yet.
    
    Add header path for imported SNRawDataProducts to ROOT_INCLUDE_DIRS
    in setup_for_development

---
+++ snemo/rdm/classes.h
@@ -0,0 +1,9 @@
+// Inclusions needed to generate a dictionary for storing
+// raw data products in Art
+
+#include "snfee/data/calo_hit_record.h"
+#include "snfee/data/has_trigger_id_interface.h"
+#include "snfee/data/tracker_hit_record.h"
+#include "snfee/data/trigger_record.h"
+
+#include "canvas/Persistency/Common/Wrapper.h"
